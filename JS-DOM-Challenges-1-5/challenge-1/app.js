479a11685d48d1fe92ecdd359b3a7d30188dd433
WAQAS AJMAL <waqasajmal092@gmail.com>
2025-02-23 19:21:16 +0500

    Challenge 1 Update

--- JS-DOM-Challenges-1-5/challenge-1/app.js
+++ JS-DOM-Challenges-1-5/challenge-1/app.js
@@ -9,7 +9,13 @@ const statusText = document.getElementById("status");
 toggleBtn.addEventListener("click", () => {
   document.body.classList.toggle("dark-mode");
   bulb.classList.toggle("off");
-  statusText.textContent = statusText.textContent.includes("Off")
-    ? "Status: On"
-    : "Status: Off";
+
+  if (statusText.textContent.includes("Off")) {
+    statusText.textContent = "Status: On";
+    toggleBtn.textContent = "Turn Off";
+  } else {
+    statusText.textContent = "Status: Off";
+    toggleBtn.textContent = "Turn On";
+  }
 });
+
